86b9675068c30ba90d0385a66ccae4f67e415586
Andrew Sichevoi <k@thekondor.net>
2023-01-15 05:11:13 +0100

    initial working implementation

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Ansible+Synopkg
+
+Basic adapter over `synopkg` to (un)install packages. Installation is with `install_from_server` only (= remote packages from a built-in list).
+
+## Usage:
+
+``` yaml
+tasks:
+    - name: Import synopkg role
+      import_role: name=ansible-role-synology-synopkg
+      
+    - name: Uninstall bloatware ActiveInsight
+      synopkg:
+        name: ActiveInsight
+        installed: no
+
+    - name: Install Docker
+      synopkg:
+        name: Docker
+        installed: yes
+```
+
---
+++ library/synopkg.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+
+from ansible.module_utils.basic import AnsibleModule
+
+import typing as t
+import subprocess
+
+
+class Synopkg(object):
+    def is_installed(self, package_name) -> bool:
+        installed = self._get_installed_packages()
+        try:
+            return installed.index(package_name)
+        except ValueError:
+            return False
+
+    def install(self, package_name) -> bytes:
+        return self._cmd("install_from_server", package_name)
+
+    def uninstall(self, package_name) -> bytes:
+        return self._cmd("uninstall", package_name)
+
+    def _get_installed_packages(self) -> t.List[str]:
+        out = self._cmd("list", "--name")
+        return out.decode("ascii").split("\n")
+
+    def _cmd(self, *args) -> bytes:
+        ### `synopkg` depends on other commands which are looked through ${PATH}
+        ### So w/o sourcing correct ${PATH}, there're weird and meaningless errors occur
+        cmd_args = " ".join(["source /etc/profile;", "synopkg", *args])
+
+        process = subprocess.Popen(cmd_args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd="/tmp")
+        rc = process.wait()
+        stdout = process.stdout.read()
+        if rc:
+            stderr = process.stderr.read()
+            raise subprocess.CalledProcessError(rc, f"{cmd_args}", stdout, stderr)
+
+        return stdout
+
+
+def run_module():
+    module_args = dict(
+        name=dict(type='str', required=True),
+        installed=dict(type='bool', required=True),
+    )
+
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
+
+    try:
+        result = _manage_package_presence(module.params['name'], module.params['installed'])
+        module.exit_json(**result)
+    except PackageManageError as e:
+        module.fail_json(msg=str(e))
+
+
+class PackageManageError(Exception): pass
+
+
+def _manage_package_presence(package_name, package_installed) -> t.Dict[str, t.Any]:
+    spkg = Synopkg()
+
+    is_installed = spkg.is_installed(package_name)
+    if package_installed:
+        if is_installed:
+            return dict(name=package_name, changed=False)
+
+        try:
+            spkg.install(package_name)
+        except subprocess.CalledProcessError as e:
+            raise PackageManageError(str(e.stdout) + " :: STDERR: " + str(e.stderr))
+
+        return dict(name=package_name, changed=True)
+
+    if not is_installed:
+        return dict(name=package_name, changed=False)
+
+    spkg.uninstall(package_name)
+    return dict(name=package_name, changed=True)
+
+
+def main():
+    run_module()
+
+
+if "__main__" == __name__:
+    main()
