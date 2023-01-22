b6a6e7efe10d04575aa11b2f6ad7e79d00250c59
kondor <k@thekondor.net>
2023-01-22 10:42:07 +0100

    Meta & Readme updated (#2)

--- README.md
+++ README.md
@@ -20,3 +20,14 @@ tasks:
         installed: yes
 ```
 
+## Notes
+
+- Tested and proven to work on `DSM 7.1`;
+- ⚠️ Package names for `synopkg` are case-sensitive (so `Docker` is an existing package name, though `docker` is not);
+- `make test` for tests;
+- The role's package is not idiomatic and this is _mostly_ intentional. You're welcome to contribute back.
+
+## Disclaimer
+
+Since the role is a part of my homelab, it has never been prepared for public distribution and further public support. Though the role suits the needs well, there is no guarantee that it will work for you or will work ever properly. You apply it on your own responsibility.
+
--- library/synopkg.py
+++ library/synopkg.py
@@ -1,4 +1,44 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+# (c) 2023, Andrew Sichevoi https://thekondor.net
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+
+DOCUMENTATION = """
+---
+module: synopkg
+short_description: Basic package management for Synology
+author: Andrew Sichevoi (@thekondor)
+notes:
+  - Packages are installed using `install_from_server`
+  - Tested and proven to work on DSM 7.1
+"""
+
+EXAMPLES = """
+- name: Uninstall bloatware ActiveInsight
+  synopkg:
+    name: ActiveInsight
+    installed: no
+
+- name: Install Docker
+  synopkg:
+    name: Docker
+    installed: yes
+"""
 
 
 from ansible.module_utils.basic import AnsibleModule
